844dbdeb1f267696af8d1b7c0c46ed67daf07ece
Boniich <ezequieldbo25@gmail.com>
2024-03-31 19:31:07 -0300

    feat: add a simple menu

--- CrudUser/Main.cpp
+++ CrudUser/Main.cpp
@@ -18,9 +18,55 @@ int main()
 {
 
     User user;
+    int option = 0;
+    bool exit = false;
+    std::string name = "";
+    int id = 0;
+    int dni = 0;
 
-    user.insertUser("Ezequiel", 36069300);
-    user.selectAllUsers();
+    std::cout << "Bienvenido a este CRUD de usuario" << std::endl;
+    while (!exit)
+    {
+        std::cout << "### Menu de opciones ###" << std::endl;
+        std::cout << "1- Leer los usuarios registrados" << std::endl;
+        std::cout << "2- Leer un usuario filtrando por ID" << std::endl;
+        std::cout << "3- Insertar un nuevo usuario" << std::endl;
+        std::cout << "4- Actualizar los datos de un usuario" << std::endl;
+        std::cout << "5- Eliminar un usuario" << std::endl;
+        std::cout << "Introduce una opcion" << std::endl;
+
+        std::cin >> option;
+
+        switch (option)
+        {
+        case 1: 
+            user.selectAllUsers();
+            break;
+        case 2:
+            
+            std::cout << "Inserta el id de un usuario: "; 
+            std::cin >> id;
+            user.selectOneUser(id);
+            break;
+        case 3:
+            std::cin >> name;
+            std::cin >> dni;
+
+            user.insertUser(name, dni);
+            break;
+
+        default:
+            break;
+        }
+
+    }
+
+
+
+    //user.insertUser("Ezequiel", 36069300);
+    //user.selectAllUsers();
+
+    
 
     //ConnectDB connect;
     //system("pause");
