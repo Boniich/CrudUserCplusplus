97e8ea9c8d5ba770ea897e09ce30e7f562bfb65a
Boniich <ezequieldbo25@gmail.com>
2024-03-31 20:13:16 -0300

    feat: add posible return if there is not user at search by id

--- CrudUser/Classes/User/User.cpp
+++ CrudUser/Classes/User/User.cpp
@@ -70,11 +70,20 @@ void User::selectOneUser(const int id) {
 	preparedStatement = con->prepareStatement("SELECT*FROM users where id = ?");
 	preparedStatement->setInt(1, id);
 	res = preparedStatement->executeQuery();
-	while (res->next()) {
-		std::cout << "ID: " << res->getInt("id")
-			<< ", Nombre: " << res->getString("name")
-			<< ", DNI; " << res->getInt("dni")
-			<< std::endl;
+
+	if (!res->next())
+	{
+		std::cout << "No existe un usuario con el ID " << id << std::endl;;
+
+	}
+	else {
+		do
+		{
+			std::cout << "ID: " << res->getInt("id")
+				<< ", Nombre: " << res->getString("name")
+				<< ", DNI; " << res->getInt("dni")
+				<< std::endl;
+		} while (res->next());
 	}
 
 	delete preparedStatement;
